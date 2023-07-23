0f787f5c7dbb876366e5cfc1676b98aa5d763cfe
Souvik Mondal <souvikmondal2120@gmail.com>
2023-07-23 21:06:49 +0530

    Switch added

--- app/src/main/java/com/kivous/composeone/OneActivity.kt
+++ app/src/main/java/com/kivous/composeone/OneActivity.kt
@@ -1,30 +1,30 @@
 package com.kivous.composeone
 
+
 import android.annotation.SuppressLint
-import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-
-
 import com.kivous.composeone.ui.theme.ComposeOneTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -62,7 +62,7 @@ class OneActivity : ComponentActivity() {
                     }
                 }, floatingActionButtonPosition = FabPosition.End
                 ) {
-                    Greeting(msg = "Hello World!!")
+                    ShowSwitch()
                 }
             }
         }
@@ -73,6 +73,24 @@ class OneActivity : ComponentActivity() {
         Text(
             text = msg
         )
+    }
+
+    @Composable
+    fun ShowSwitch() {
+        val isChecked = remember {
+            mutableStateOf(false)
+        }
+
+        Switch(
+            checked = isChecked.value,
+            onCheckedChange = {
+                isChecked.value = it
+            },
+            modifier = Modifier.run {
+                padding(24.dp, 80.dp)
+            }
+        )
+
     }
 
 }
